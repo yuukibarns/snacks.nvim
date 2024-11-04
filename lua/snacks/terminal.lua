a4af729b2489714b066ca03f008bf5fe42c93343
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-04 10:02:38 +0100

    feat(terminal): better double esc to go to normal mode

--- lua/snacks/terminal.lua
+++ lua/snacks/terminal.lua
@@ -24,6 +24,22 @@ local defaults = {
           vim.cmd("e " .. f)
         end
       end,
+      term_normal = {
+        "<esc>",
+        function(self)
+          self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+          if self.esc_timer:is_active() then
+            self.esc_timer:stop()
+            vim.cmd("stopinsert")
+          else
+            self.esc_timer:start(200, 0, function() end)
+            return "<esc>"
+          end
+        end,
+        mode = "t",
+        expr = true,
+        desc = "Double escape to normal mode",
+      },
     },
   },
 }
