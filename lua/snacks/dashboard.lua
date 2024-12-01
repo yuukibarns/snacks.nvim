4bdf7daece384ab8a5472e6effd5fd6167a5ce6a
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-01 19:56:43 +0100

    fix(dashboard): truncate file names when too long. Fixes #183

--- lua/snacks/dashboard.lua
+++ lua/snacks/dashboard.lua
@@ -116,6 +116,14 @@ local defaults = {
     file = function(item, ctx)
       local fname = vim.fn.fnamemodify(item.file, ":~")
       fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+      if #fname > ctx.width then
+        local dir = vim.fn.fnamemodify(fname, ":h")
+        local file = vim.fn.fnamemodify(fname, ":t")
+        if dir and file then
+          file = file:sub(-(ctx.width - #dir - 2))
+          fname = dir .. "/…" .. file
+        end
+      end
       local dir, file = fname:match("^(.*)/(.+)$")
       return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
     end,
