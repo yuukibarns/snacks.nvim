74feefc52284e2ebf93ad815ec5aaeec918d4dc2
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-17 23:46:04 +0100

    feat(picker.smart): better frecency bonus

--- lua/snacks/picker/source/smart.lua
+++ lua/snacks/picker/source/smart.lua
@@ -3,6 +3,9 @@ local M = {}
 ---@class snacks.picker
 ---@field smart fun(opts?: snacks.picker.smart.Config): snacks.picker.finder
 
+local BONUS_FRECENCY = 8
+local BONUS_CWD = 10
+
 ---@param opts snacks.picker.smart.Config
 ---@type snacks.picker.finder
 function M.smart(opts, filter)
@@ -16,9 +19,10 @@ function M.smart(opts, filter)
       return false
     end
     done[path] = true
-    local score = freceny:get(item)
+    local score = (1 - 1 / (1 + freceny:get(item))) * BONUS_FRECENCY
+    item.frecency = score
     if path:find(cwd, 1, true) then
-      score = score + 20
+      score = score + BONUS_CWD
     end
     item.score_add = score
   end)(opts, filter)
