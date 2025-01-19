007989da3df7e163a55638ff0d86268401cd1cef
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-19 12:06:37 +0100

    style(indent): remove unused options

--- lua/snacks/indent.lua
+++ lua/snacks/indent.lua
@@ -74,11 +74,6 @@ local defaults = {
       arrow = ">",
     },
   },
-  blank = {
-    char = " ",
-    -- char = "·",
-    hl = "SnacksIndentBlank", ---@type string|string[] hl group for blank spaces
-  },
   -- filter for buffers to enable indent guides
   filter = function(buf)
     return vim.g.snacks_indent ~= false and vim.b[buf].snacks_indent ~= false and vim.bo[buf].buftype == ""
