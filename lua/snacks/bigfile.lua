08852ac7f811f51d47a590d62df805d0e84e611a
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-28 09:10:17 +0100

    fix(bigfile): check if buf still exists when applying syntax. Fixes #737

--- lua/snacks/bigfile.lua
+++ lua/snacks/bigfile.lua
@@ -21,7 +21,9 @@ local defaults = {
     Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
     vim.b.minianimate_disable = true
     vim.schedule(function()
-      vim.bo[ctx.buf].syntax = ctx.ft
+      if vim.api.nvim_buf_is_valid(ctx.buf) then
+        vim.bo[ctx.buf].syntax = ctx.ft
+      end
     end)
   end,
 }
