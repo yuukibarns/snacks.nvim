89fcb3bb2025cb1c986e9af3478715f6e0bdf425
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-30 19:48:59 +0100

    fix(picker.explorer): stop file follow when picker was closed

--- lua/snacks/picker/source/explorer.lua
+++ lua/snacks/picker/source/explorer.lua
@@ -50,7 +50,7 @@ function State:follow()
     return
   end
   local picker = self.picker()
-  if not picker or picker:is_focused() then
+  if not picker or picker:is_focused() or picker.closed then
     return
   end
   local buf = vim.api.nvim_get_current_buf()
