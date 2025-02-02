5a2056575faa50f788293ee787b803c15f42a9e0
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-02-02 22:04:36 +0100

    feat(picker.projects): enabled frecency for projects picker

--- lua/snacks/picker/config/sources.lua
+++ lua/snacks/picker/config/sources.lua
@@ -621,6 +621,12 @@ M.projects = {
   confirm = "load_session",
   patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "package.json", "Makefile" },
   recent = true,
+  matcher = {
+    frecency = true, -- use frecency boosting
+    sort_empty = true, -- sort even when the filter is empty
+    cwd_bonus = false,
+  },
+  sort = { fields = { "score:desc", "idx" } },
   win = {
     preview = { minimal = true },
     input = {
