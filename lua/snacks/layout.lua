27256cf989475e3305713341930a7709e3670eac
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-02-14 12:38:43 +0100

    fix(layout): deep merge instead of shallow merge for window options. Closes #1166

--- lua/snacks/layout.lua
+++ lua/snacks/layout.lua
@@ -438,8 +438,7 @@ function M:update_win(win, parent)
   w.enabled = true
   assert(w, ("win %s not part of layout"):format(win.win))
   -- add win opts from layout
-  w.opts = vim.tbl_extend(
-    "force",
+  w.opts = Snacks.config.merge(
     vim.deepcopy(self.win_opts[win.win] or {}),
     {
       width = 0,
