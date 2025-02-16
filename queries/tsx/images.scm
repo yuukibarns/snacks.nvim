a8b3c20fbc814a2e0a599761e19fdc27470e24ed
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-02-16 15:10:42 +0100

    refactor(image): make tsx inherit from javascript

--- queries/tsx/images.scm
+++ queries/tsx/images.scm
@@ -1,18 +1,1 @@
-
-(jsx_element
-  (jsx_opening_element
-    (identifier) @tag (#eq? @tag "img")
-    (jsx_attribute
-      (property_identifier) @attr_name (#eq? @attr_name "src")
-      (string (string_fragment) @image)
-    )
-  )
-) @anchor
-
-(jsx_self_closing_element
-  (identifier) @tag (#eq? @tag "img")
-  (jsx_attribute
-    (property_identifier) @attr_name (#eq? @attr_name "src")
-    (string (string_fragment) @image)
-  )
-) @anchor
+; inherits: javascript
