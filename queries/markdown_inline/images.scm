beaa1c2efcc598a9752b4536ef95606a10835aaa
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-02-19 08:50:03 +0100

    fix(image): fixed gsub for angle brackets. Closes #1301

--- queries/markdown_inline/images.scm
+++ queries/markdown_inline/images.scm
@@ -5,5 +5,5 @@
     (image_description (shortcut_link (link_text) @image.src))
     (#gsub! @image.src "|.*" "") ; remove wikilink image options
     (#gsub! @image.src "^<" "") ; remove bracket link
-    (#gsub! @image.src "^>" "")
+    (#gsub! @image.src ">$" "")
   ]) @image
