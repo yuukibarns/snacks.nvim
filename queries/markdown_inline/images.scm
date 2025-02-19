830ac62815e70f3db35ed7a295710c836578c6e3
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-02-19 08:52:39 +0100

    fix(image.markdown): fix image treesitter query. Closes #1300

--- queries/markdown_inline/images.scm
+++ queries/markdown_inline/images.scm
@@ -3,7 +3,8 @@
   [
     (link_destination) @image.src
     (image_description (shortcut_link (link_text) @image.src))
+  ]
     (#gsub! @image.src "|.*" "") ; remove wikilink image options
     (#gsub! @image.src "^<" "") ; remove bracket link
     (#gsub! @image.src ">$" "")
-  ]) @image
+  ) @image
