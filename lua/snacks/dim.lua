47e1440d547233772a3958580d429b38b5959edd
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-02-20 17:17:16 +0100

    fix(dim): check that win is valid when animating dim. Closes #1342

--- lua/snacks/dim.lua
+++ lua/snacks/dim.lua
@@ -110,7 +110,7 @@ function M.enable(opts)
         end
 
         Snacks.animate(scopes_anim[win].from, scope.from, function(v)
-          if not scopes_anim[win] then
+          if not scopes_anim[win] or not vim.api.nvim_win_is_valid(win) then
             return
           end
           scopes_anim[win].from = v
@@ -118,7 +118,7 @@ function M.enable(opts)
         end, vim.tbl_extend("keep", { int = true, id = "snacks_dim_from_" .. win, buf = buf }, opts.animate))
 
         Snacks.animate(scopes_anim[win].to, scope.to, function(v)
-          if not scopes_anim[win] then
+          if not scopes_anim[win] or not vim.api.nvim_win_is_valid(win) then
             return
           end
           scopes_anim[win].to = v
