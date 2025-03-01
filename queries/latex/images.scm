714d7616f0b76ff7c099b5604b19a1a6ab909511
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-03-01 07:54:48 +0100

    fix(image.latex): don't nest image nodes

--- queries/latex/images.scm
+++ queries/latex/images.scm
@@ -1,7 +1,15 @@
-(inline_formula) @image.content @image
+(inline_formula
+  (#set! image.ext "math.tex"))
+  @image.content @image
 
-(displayed_equation) @image.content @image
-(math_environment) @image.content @image
+(displayed_equation
+  (#set! image.ext "math.tex"))
+  @image.content @image
+
+((math_environment
+  (#set! image.ext "math.tex"))
+  @image.content @image
+  (#not-has-ancestor? @image "displayed_equation"))
 
 (graphics_include
   (_ (path) @image.src)
